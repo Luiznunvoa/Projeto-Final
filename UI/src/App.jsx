7b5b671fd82cd30a7a53d770c0ea359264e3e2ff
Luiz Gabriel Moraes <luizgpinheiro0905@gmail.com>
2024-08-17 11:01:20 -0300

    feat(App.jsx): Adição da indtrodução da página principal

--- UI/src/App.jsx
+++ UI/src/App.jsx
@@ -1,10 +1,17 @@
 import {Header} from "./components/Header.jsx"
 import {Footer} from "./components/Footer.jsx"
+import styles from './app.module.css'
 import './global.css'
 export function App() {
   return (
     <main>
       <Header />
+      <section className={styles.top}>
+        <section>
+          <h1>Transformando Filmes em Experiências Personalizadas</h1>
+          <p>Reserve Seu Assento e Viva a Magia do Cinema!</p>
+        </section>
+      </section>
       <Footer />
     </main>
   )
