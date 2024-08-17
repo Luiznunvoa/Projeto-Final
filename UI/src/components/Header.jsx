7fa0b403eb6dc2021d1b6cf3c47b34470aa06ee8
Luiz Gabriel Moraes <luizgpinheiro0905@gmail.com>
2024-08-17 00:52:44 -0300

    feat(UI): finalização da estilização do cabeçalho

--- UI/src/components/Header.jsx
+++ UI/src/components/Header.jsx
@@ -1,10 +1,25 @@
 import styles from './header.module.css'
 import Icon from '../assets/Logo.svg';
+import Movies from '../assets/Movies.svg'
+import Sign from '../assets/Sign.svg'
+import Help from '../assets/Help.svg'
 
 export function Header(){
    return(
       <header>
             <img src={Icon} alt="Logo"/>
+            <section>
+               <a href='https://www.youtube.com/watch?v=dQw4w9WgXcQ'>
+                  <img src={Movies} alt="Movies"/>
+               </a>
+               <a href='https://www.youtube.com/watch?v=dQw4w9WgXcQ'>
+                  <img src={Sign} alt="Sign"/>
+               </a>
+               <a href='https://www.youtube.com/watch?v=dQw4w9WgXcQ'>
+                  <img src={Help} alt="Help"/>
+               </a>
+
+            </section>
       </header>
 
    )
