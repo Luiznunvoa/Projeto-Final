58e1ad579c76ead580a8099ca470337ceda91c24
Gabriel Camara <galvares439@gmail.com>
2024-08-17 16:43:58 -0300

    Adicionado o middleware checkUserExistsMiddleware

---
+++ API/src/middlewares/checkUserExistsMiddleware.js
@@ -0,0 +1,19 @@
+const { User } = require('../models/models')
+
+async function checkUserExists(req, res, next) {
+     try {
+          const { username } = req.body
+          const user = await User.findOne({
+               where: {
+                    username
+               }
+          })
+          if (user) {
+               return res.status(400).json({ error: 'Usuário ja existe!'})
+          }
+
+          return next()
+     } catch (error) {
+          return res.status(500).json({ error: 'Erro ao checar se o usuário existe!'})
+     }
+}
