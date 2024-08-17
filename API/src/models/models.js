30c4e8fa40149185021ab735d165fd543234ff6b
Ayrton Surica <ayrtonsurica@gmail.com>
2024-08-17 16:16:15 -0300

    feat(models): criação dos modelos para o banco de dados

--- API/src/models/models.js
+++ API/src/models/models.js
@@ -1,5 +1,113 @@
 const sequelize = require("../../config/database.js")
+const { DataTypes } = require("sequelize")
 
 const Movie = sequelize.define("Movie", {
-
+    title: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    imageURL: {
+        type: DataTypes.TEXT,
+        allowNull: false
+    },
+    synopse: {
+        type: DataTypes.TEXT,
+        allowNull: false
+    },
+    genre: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    ageRating: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    director: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
 })
+
+const Section = sequelize.define("Section", {
+    time: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    city: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    neighborhood: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    type: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    }
+})
+
+const Seat = sequelize.define("Seat", {
+    number: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    row: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    price: {
+        type: DataTypes.FLOAT,
+        allowNull: false
+    },
+    ocupierCPF: {
+        type: DataTypes.STRING
+    },
+    ocupierName: {
+        type: DataTypes.STRING
+    }
+})
+
+const User = sequelize.define("User", {
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }, 
+    lastName: {
+        type: DataTypes.STRING,
+        allowNull: null
+    }, 
+    cpf: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }, 
+    birthday: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }, 
+    username: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }, 
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }, 
+    senha: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+})
+
+Movie.hasMany(Section, {onDelete: "CASCADE", foreignKey: "title"})
+Section.belongsTo(Movie)
+
+Section.hasMany(Seat, {onDelete: "CASCADE", foreignKey: "time"})
+Seat.belongsTo(Section)
+
+module.exports = {
+    Movie,
+    Section,
+    Seat,
+    User
+}
