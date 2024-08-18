12d06087a801e9a5bc94a51b0ceb884dc72615c9
Luiz Gabriel Moraes <luizgpinheiro0905@gmail.com>
2024-08-18 20:57:20 -0300

    feat(App.jsx): adição dos 3 tickets

--- UI/src/App.jsx
+++ UI/src/App.jsx
@@ -17,8 +17,27 @@ export function App() {
       <section className={styles.mid}>
         <div className={styles.back}>
           <section className={styles.carrousel}>
-            <Combos />
-            
+            <Combos 
+              nome="Pipoca (P)" 
+              preco = "11,99" 
+              imagem="/src/assets/Pipoca.svg" 
+              desc="Nada diz 'cinema' como o aroma inconfundível de pipoca estourando. E quando se trata de uma porção de pipoca pequena, a magia se torna ainda mais irresistível. Imagine uma explosão de sabores em miniatura, onde cada grão é um pedacinho de felicidade."
+              seta='1'
+            />
+            <Combos 
+              nome="Pipoca (G)" 
+              preco = "13,99" 
+              imagem="/src/assets/Pipoca.svg" 
+              desc="No reino da experiência cinematográfica, uma rainha reina supremamente: a pipoca grande, uma verdadeira protagonista em cada tela de prata. Ela transcende o mero lanche e se transforma em uma jornada culinária que rivaliza com as tramas mais emocionantes."
+              seta="2"
+            />
+            <Combos 
+              nome="Duo Pipoca (M) + Refri" 
+              preco = "16,99" 
+              imagem="/src/assets/Promo.svg" 
+              desc="O 'Duo Pipoca + Refri' é mais do que um lanche; é uma tradição que traz à tona as melhores lembranças de ir ao cinema. Compartilhado entre amigos, dividido com a família ou desfrutado em momentos de autocuidado, este combo é um símbolo da felicidade encontrada nos momentos mais simples."
+              seta="1"
+            />
           </section>
         </div>
 
--- UI/src/components/Combos.jsx
+++ UI/src/components/Combos.jsx
@@ -1,15 +1,24 @@
-import styles from './combos.module.css'
-export function Combos(){
-   return(
-      <>
+import React, { useState } from 'react';
+import styles from './combos.module.css';
+
+export function Combos(props) {
+  const [currentSeta, setCurrentSeta] = useState(props.seta);
+
+  return (
+    <>
       <div className={styles.combo}>
-         <div className={styles.desc}>
-            
-         </div>
-
+        <div className={styles.desc}>
+          <div className={currentSeta === '1' ? styles.arrow1 : styles.arrow2}>
+            {/* Conteúdo opcional da seta */}
+          </div>
+          <img src={props.imagem} alt={props.nome} />
+          <section>
+            <h1>{props.nome}</h1>
+            <p>R$ {props.preco}</p>
+            <p>{props.desc}</p>
+          </section>
+        </div>
       </div>
-
-      </>
-
-   )
+    </>
+  );
 }
