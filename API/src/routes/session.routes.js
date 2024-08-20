66166f4b0f7c23f09fc67a6579dfdae2b5e3a556
Victor Bastos <vbasx@outlook.com>
2024-08-20 17:52:24 -0300

    feat: add routes

---
+++ API/src/routes/session.routes.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const sessionRoutes = express.Router();
+
+// importar controller
+const sessionController = require("../controllers/sessionController");
+
+// importar middleware
+const findSessionById = require("../middlewares/findSessionMiddleware");
+
+// criar sessão
+sessionRoutes.post("/", (req, res) => sessionController.createSession(req, res));
+
+// listar sessões
+sessionRoutes.get("/", (req, res) => sessionController.listSessions(req, res));
+
+// buscar sessão por id
+sessionRoutes.get("/:id", findSessionById, (req, res) => sessionController.getSessionById(req, res));
+
+// editar sessão
+sessionRoutes.patch("/:id", findSessionById, (req, res) => sessionController.updateSession(req, res));
+
+// deletar sessão
+sessionRoutes.delete("/:id", findSessionById, (req, res) => sessionController.deleteSession(req, res));
+
+module.exports = sessionRoutes;
