74f9f232af40a9bf1f6523cd5e04ca8350bbb433
Victor Bastos <vbasx@outlook.com>
2024-08-20 18:37:06 -0300

    feat: add parte das sessões ao index

--- API/src/routes/index.js
+++ API/src/routes/index.js
@@ -1,12 +1,13 @@
-const express = require('express')
+const Router = require("express");
 
-const userRoutes = require('./user.routes')
-const movieRoutes = require('./movie.routes')
-const seatRoutes = require('./seat.routes')
-const sectionRoutes = require('./section.routes')
+const userRoutes = require('./user.routes');
+const movieRoutes = require('./movie.routes');
+const seatRoutes = require('./seat.routes');
+const sectionRoutes = require('./section.routes');
 
-const router = express.Router()
+const router = Router();
 
-router.use('/users', userRoutes)
+router.use('/users', userRoutes);
+router.use('/seats', seatRoutes);
 
-module.exports = router
+module.exports = router;
