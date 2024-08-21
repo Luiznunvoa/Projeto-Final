e5b00588d2d17f2c32edbab9629dcb3c913b0a2a
Luiz Gabriel Moraes <luizgpinheiro0905@gmail.com>
2024-08-21 14:56:38 -0300

    feat(Login.jsx): adição de página de Login

--- UI/src/App.jsx
+++ UI/src/App.jsx
@@ -2,6 +2,7 @@ import { Home } from "./pages/Home";
 import { Movies } from "./pages/Moveis";
 import { Checkout } from "./pages/Checkout";
 import { Contact } from "./pages/Contact";
+import { Login } from "./pages/Login";
 import Layout from './Layout';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
@@ -10,7 +11,8 @@ export function App() {
     <Router>
         <Layout>
             <Routes>
-                <Route path="/" element={<Home />} />
+                <Route path="/" element={<Login />} />
+                <Route path="/Home" element={<Home />} />
                 <Route path="/Movies" element={<Movies />} />
                 <Route path="/Checkout" element={<Checkout />} />
                 <Route path="/Contact" element={<Contact />} />
---
+++ UI/src/pages/Login.jsx
@@ -0,0 +1,39 @@
+import styles from './login.module.css';
+import '../global.css';
+import Icon from '../assets/Logo.svg';
+export function Login() {
+    return (
+        <>
+        <main className={styles.login}>
+         <section className={styles.mid}>
+            <section className={styles.intro}>
+               <img src={Icon} alt="Logo"/>
+               <h2>
+                  Escolha suas sessões, reserve seus lugares e 
+                  entre de cabeça em narrativas que cativam e 
+                  emocionam. Este é o nosso convite para você 
+                  vivenciar o cinema de maneira única. Nossa 
+                  página de login é a porta de entrada para 
+                  essa experiência excepcional, e estamos 
+                  empolgados para compartilhar cada momento 
+                  cinematográfico com você.
+               </h2>
+            </section>
+            <div className={styles.form}>
+               <h1>Login</h1>
+               <h2>Faça seu login e garanta o seu lugar na diversão!</h2>
+               <input className={styles.user} type='text' placeholder='Usuário ou E-mail'/>
+               <input className={styles.user} type='password' placeholder='Senha'/>
+               <div className={styles.enter}><h2>ENTRAR</h2></div>
+               <hr/>
+               <div className={styles.register}>
+                  <h2>CADASTRE-SE</h2>
+               </div>
+            </div>
+         </section>
+
+        </main>
+
+        </>
+    );
+}
