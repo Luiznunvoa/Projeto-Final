f803b8d846eb063e32ca2a7253f8e74ff73976ae
Luiz Gabriel Moraes <luizgpinheiro0905@gmail.com>
2024-08-21 18:37:24 -0300

    fix(Movies.jsx): Simplificação do Movies.jsx

--- UI/src/pages/Moveis.jsx
+++ UI/src/pages/Moveis.jsx
@@ -6,75 +6,88 @@ import '../global.css';
 
 import capa1Image from '../assets/Capa1.png';
 
+const filmes = [
+    {
+        imagem: capa1Image,
+        nome: 'BESOURO AZUL',
+        genero: 'Ação/Aventura',
+        diretor: 'Angel Manuel Soto',
+        desc: 'Quando um escaravelho alienígena se funde com seu corpo, Jaime ganha uma armadura tecnológica que lhe concede superpoderes incríveis.'
+    },
+    {
+      imagem: capa1Image,
+      nome: 'BESOURO AZUL',
+      genero: 'Ação/Aventura',
+      diretor: 'Angel Manuel Soto',
+      desc: 'Quando um escaravelho alienígena se funde com seu corpo, Jaime ganha uma armadura tecnológica que lhe concede superpoderes incríveis.'
+   },
+   {
+      imagem: capa1Image,
+      nome: 'BESOURO AZUL',
+      genero: 'Ação/Aventura',
+      diretor: 'Angel Manuel Soto',
+      desc: 'Quando um escaravelho alienígena se funde com seu corpo, Jaime ganha uma armadura tecnológica que lhe concede superpoderes incríveis.'
+   },
+   {
+      imagem: capa1Image,
+      nome: 'BESOURO AZUL',
+      genero: 'Ação/Aventura',
+      diretor: 'Angel Manuel Soto',
+      desc: 'Quando um escaravelho alienígena se funde com seu corpo, Jaime ganha uma armadura tecnológica que lhe concede superpoderes incríveis.'
+   },
+   {
+      imagem: capa1Image,
+      nome: 'BESOURO AZUL',
+      genero: 'Ação/Aventura',
+      diretor: 'Angel Manuel Soto',
+      desc: 'Quando um escaravelho alienígena se funde com seu corpo, Jaime ganha uma armadura tecnológica que lhe concede superpoderes incríveis.'
+   },
+   {
+      imagem: capa1Image,
+      nome: 'BESOURO AZUL',
+      genero: 'Ação/Aventura',
+      diretor: 'Angel Manuel Soto',
+      desc: 'Quando um escaravelho alienígena se funde com seu corpo, Jaime ganha uma armadura tecnológica que lhe concede superpoderes incríveis.'
+   },
+    
+];
+
 export function Movies() {
     return (
-        <>
-         <main className={styles.movies}>
-            <section className={styles.search}>
-               <div className={styles.searchbar}>
-                  <input type="text" placeholder="Pesquisar filmes" />                  
-                  <div />
-               </div>
-            <section className={styles.dropdowns}>
-               <Dropdown tipo='Gênero' />
-               <Dropdown tipo='Classificação' />
-            </section>
-
-            </section>
+        <main className={styles.movies}>
+            <div className={styles.search}>
+                <section className={styles.searchbar}>
+                    <input type="text" placeholder="Pesquisar filmes" />
+                    <div />
+                </section>
+                <section className={styles.dropdowns}>
+                    <Dropdown tipo='Gênero' />
+                    <Dropdown tipo='Classificação' />
+                </section>
+            </div>
             <section className={styles.mid}>
-               <h1 className={styles.title}>Filmes</h1>
-               <section className={styles.list}>
-                  <Card imagem={capa1Image}
-                           nome='BESOURO AZUL'
-                           genero='Ação/Aventura'
-                           diretor='Angel Manuel Soto'
-                           desc='Quando um escaravelho alienígena se funde com seu corpo, Jaime ganha uma armadura tecnológica que lhe concede superpoderes incríveis.'
-                     />
-                  <Card imagem={capa1Image}
-                           nome='BESOURO AZUL'
-                           genero='Ação/Aventura'
-                           diretor='Angel Manuel Soto'
-                           desc='Quando um escaravelho alienígena se funde com seu corpo, Jaime ganha uma armadura tecnológica que lhe concede superpoderes incríveis.'
-                     />
-                  <Card imagem={capa1Image}
-                           nome='BESOURO AZUL'
-                           genero='Ação/Aventura'
-                           diretor='Angel Manuel Soto'
-                           desc='Quando um escaravelho alienígena se funde com seu corpo, Jaime ganha uma armadura tecnológica que lhe concede superpoderes incríveis.'
-                     />
-                  <Card imagem={capa1Image}
-                           nome='BESOURO AZUL'
-                           genero='Ação/Aventura'
-                           diretor='Angel Manuel Soto'
-                           desc='Quando um escaravelho alienígena se funde com seu corpo, Jaime ganha uma armadura tecnológica que lhe concede superpoderes incríveis.'
-                     />
-                  <Card imagem={capa1Image}
-                           nome='BESOURO AZUL'
-                           genero='Ação/Aventura'
-                           diretor='Angel Manuel Soto'
-                           desc='Quando um escaravelho alienígena se funde com seu corpo, Jaime ganha uma armadura tecnológica que lhe concede superpoderes incríveis.'
-                     />
-                  <Card imagem={capa1Image}
-                           nome='BESOURO AZUL'
-                           genero='Ação/Aventura'
-                           diretor='Angel Manuel Soto'
-                           desc='Quando um escaravelho alienígena se funde com seu corpo, Jaime ganha uma armadura tecnológica que lhe concede superpoderes incríveis.'
-                     />
-               </section>
-               <section className={styles.pages}>
-                  <div className={styles.arrowL}>
-                     <div />
-                  </div>
-                  <div className={styles.arrowR}>
-                     <div />
-                  </div>
-
-               </section>
-
+                <h1>Filmes</h1>
+                <section className={styles.list}>
+                    {filmes.map((filme, index) => (
+                        <Card
+                            key={index}
+                            imagem={filme.imagem}
+                            nome={filme.nome}
+                            genero={filme.genero}
+                            diretor={filme.diretor}
+                            desc={filme.desc}
+                        />
+                    ))}
+                </section>
+                <section className={styles.pages}>
+                    <div className={styles.arrowL}>
+                        <div />
+                    </div>
+                    <div className={styles.arrowR}>
+                        <div />
+                    </div>
+                </section>
             </section>
-            
-            
-         </main>
-        </>
+        </main>
     );
 }
