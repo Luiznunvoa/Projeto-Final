bebb7abbec5182d3948f1272ec41f701ef0d4458
Luiz Gabriel Moraes <luizgpinheiro0905@gmail.com>
2024-08-23 16:16:10 -0300

    fix(Home.jsx): ajuste noo caminho da informação

--- UI/src/components/Movie.jsx
+++ UI/src/components/Movie.jsx
@@ -1,13 +1,16 @@
 import styles from './movie.module.css'
 import { Link } from "react-router-dom";
+import { ApplicationContext } from "../contexts/ApplicationContextProvider";
+import { useContext } from "react"
 
 export function Movie(props){
+   const { title , setTitle} = useContext(ApplicationContext)
    return(
       <>
       <section className={styles.movie}>
          <img src={props.imagem}></img>
          <h3>{props.nome}</h3>
-         <Link className={styles.button}to='/Sessions'>
+         <Link className={styles.button}to='/Sessions' onClick={() => setTitle(props.nome)}>
             <h3>SESSÔES DISPOÍVEIS</h3>
          </Link>
 
